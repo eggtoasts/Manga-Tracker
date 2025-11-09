00e045e302b145295706da6c8c46e8a90c473630
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-11-08 22:28:56 -0500

    Add review box, emotion emojis, star rating to review form

--- frontend/src/components/AddRecForm.jsx
+++ frontend/src/components/AddRecForm.jsx
@@ -1,26 +1,112 @@
+import { Angry, Frown, Laugh, Meh, MoveLeft, Smile, Star } from "lucide-react";
+import { useState } from "react";
+
 export default function AddRecForm({
   closeDialog,
   selectedManga,
   setSelectedManga,
 }) {
+  const [emotion, setEmotion] = useState("Peak Fiction");
   const manga = selectedManga.manga;
   console.log(selectedManga);
+
+  //oooh i got some ideas
+  // -recomended, NOT recomended, mixed
+
+  //how many chapters read (finished, dropped)
+  // - favorite character section
   return (
     <>
       {/* form of the selected manga*/}
 
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-        <div className="rounded p-3 bg-white fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10">
-          <button
-            onClick={() => setSelectedManga({ selected: false, object: {} })}
-          >
-            dummy back button
-          </button>
-
+        <div className="w-190 rounded p-3 bg-white fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10">
           {/* header */}
           <div>
-            <h1 className="text-lg font-semibold">Add Your Recommendation</h1>
-            <h2>Share your thoughts about {manga.name}</h2>
+            <div className="flex text-lg font-semibold pb-1 border-b-1 border-gray-300">
+              <p>Add Your Review</p>
+              <button
+                className="hover:cursor-pointer flex items-center justify-center gap-2 ml-auto"
+                onClick={() =>
+                  setSelectedManga({ selected: false, object: {} })
+                }
+              >
+                <MoveLeft size={10} />{" "}
+                <p className=" font-medium text-xs justify-centerflex items-center ">
+                  Go back
+                </p>
+              </button>
+            </div>
+          </div>
+
+          {/* the manga chosen */}
+          <div className="flex pt-3">
+            <img
+              className="mx-3 rounded-xl w-36 h-54 object-cover"
+              src={manga.image}
+            ></img>
+
+            <div className="px-5 w-full flex flex-col [&>h1]:pb-1">
+              {/* manga title */}
+              <h1 className="text-xl font-bold">{manga.name}</h1>
+
+              {/* emotions felt */}
+              <h1 className="text-sm font-bold">Would you recommend this?</h1>
+              <div className="text-xs flex gap-2">
+                <button className="border rounded-4xl px-[3px] py-[2px]  flex items-center justify-center gap-1">
+                  <Laugh size={15} /> <p className="w-max">Peak Fiction</p>
+                </button>
+                <button className="flex items-center gap-1">
+                  <Smile size={15} /> <p className="w-max">Recommended</p>
+                </button>
+
+                <button className=" flex items-center gap-1">
+                  <Meh size={15} /> <p className="w-max">Mixed Feelings</p>
+                </button>
+
+                <button className="flex items-center justify-center gap-1">
+                  <Frown size={15} /> <p className="w-max">Not Recommended</p>
+                </button>
+              </div>
+
+              {/* star rating */}
+              <h1 className="text-sm font-bold">Overall Rating</h1>
+              <div className="flex">
+                {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((a) => (
+                  <Star size={20} />
+                ))}
+              </div>
+
+              {/* spoiler warning */}
+              <h1 className="text-sm font-bold">Spoiler Warning</h1>
+              <div className="inline text-sm">
+                <label className="">
+                  yes
+                  <input className="ml-1" type="radio" name="r" />
+                </label>
+
+                <label className="ml-2">
+                  no
+                  <input
+                    className="ml-1"
+                    type="radio"
+                    name="r"
+                    defaultChecked
+                  />
+                </label>
+              </div>
+
+              <textarea
+                placeholder="Add a review..."
+                className="focus:outline-none border-gray-300 border rounded mb-2 text-sm p-2 h-40 w-full resize-none mt-2"
+              />
+            </div>
+          </div>
+
+          <div className="w-full bg-gray-200">
+            <button className="ml-auto hover:cursor-pointer rounded-md font-medium p-2 text-xs justify-center bg-black text-amber-50 flex items-center min-w-20">
+              Submit
+            </button>
           </div>
         </div>
       </div>
